38b8affb8bdcd66da0905eced9af30c5a1c97ed5
Przemysław Żydek <przemyslawzydek@gmail.com>
2022-08-10 12:55:19 +0100

    chore: add initial go files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module fingerprint-pro-server-api-go-sdk
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	printHelloWorld()
+}
+
+func printHelloWorld() {
+	fmt.Println("Hello, World!")
+}
