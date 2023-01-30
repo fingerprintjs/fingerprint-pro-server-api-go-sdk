3988bf62a50aaa48acaa27b58e12997a021a48d0
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-01-30 13:46:22 +0100

    fix: bump version in module name to v3

--- test/utils.go
+++ test/utils.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"os"
 
-	"github.com/fingerprintjs/fingerprint-pro-server-api-go-sdk/v2/sdk"
+	"github.com/fingerprintjs/fingerprint-pro-server-api-go-sdk/v3/sdk"
 )
 
 func readFromFileAndUnmarshal(path string, i interface{}) {
