ba9e1b58525bca1e6abd464d6f0f09f836703d8a
Necip Allef <necipallef@gmail.com>
2024-03-22 18:29:58 +0300

    test: tests for empty sealed result and empty key

--- test/Unseal_test.go
+++ test/Unseal_test.go
@@ -53,6 +53,30 @@ func TestUnsealEventsResponse(t *testing.T) {
 		assert.True(t, isEqual)
 	})
 
+	t.Run("with empty result", func(t *testing.T) {
+		key := base64Decode("p2PA7MGy5tx56cnyJaFZMr96BCFwZeHjZV2EqMvTq53=")
+		_, err := sealed.UnsealEventsResponse([]byte(""), []sealed.DecryptionKey{
+			{
+				Key:       key,
+				Algorithm: sealed.AlgorithmAES256GCM,
+			},
+		})
+		assert.Error(t, err)
+		assert.Equal(t, "invalid sealed data header", err.Error())
+	})
+
+	t.Run("with empty key", func(t *testing.T) {
+		sealedResult := base64Decode("noXc7SXO+mqeAGrvBMgObi/S0fXTpP3zupk8qFqsO/1zdtWCD169iLA3VkkZh9ICHpZ0oWRzqG0M9/TnCeKFohgBLqDp6O0zEfXOv6i5q++aucItznQdLwrKLP+O0blfb4dWVI8/aSbd4ELAZuJJxj9bCoVZ1vk+ShbUXCRZTD30OIEAr3eiG9aw00y1UZIqMgX6CkFlU9L9OnKLsNsyomPIaRHTmgVTI5kNhrnVNyNsnzt9rY7fUD52DQxJILVPrUJ1Q+qW7VyNslzGYBPG0DyYlKbRAomKJDQIkdj/Uwa6bhSTq4XYNVvbk5AJ/dGwvsVdOnkMT2Ipd67KwbKfw5bqQj/cw6bj8Cp2FD4Dy4Ud4daBpPRsCyxBM2jOjVz1B/lAyrOp8BweXOXYugwdPyEn38MBZ5oL4D38jIwR/QiVnMHpERh93jtgwh9Abza6i4/zZaDAbPhtZLXSM5ztdctv8bAb63CppLU541Kf4OaLO3QLvfLRXK2n8bwEwzVAqQ22dyzt6/vPiRbZ5akh8JB6QFXG0QJF9DejsIspKF3JvOKjG2edmC9o+GfL3hwDBiihYXCGY9lElZICAdt+7rZm5UxMx7STrVKy81xcvfaIp1BwGh/HyMsJnkE8IczzRFpLlHGYuNDxdLoBjiifrmHvOCUDcV8UvhSV+UAZtAVejdNGo5G/bz0NF21HUO4pVRPu6RqZIs/aX4hlm6iO/0Ru00ct8pfadUIgRcephTuFC2fHyZxNBC6NApRtLSNLfzYTTo/uSjgcu6rLWiNo5G7yfrM45RXjalFEFzk75Z/fu9lCJJa5uLFgDNKlU+IaFjArfXJCll3apbZp4/LNKiU35ZlB7ZmjDTrji1wLep8iRVVEGht/DW00MTok7Zn7Fv+MlxgWmbZB3BuezwTmXb/fNw==")
+		_, err := sealed.UnsealEventsResponse(sealedResult, []sealed.DecryptionKey{
+			{
+				Key:       base64Decode(""),
+				Algorithm: sealed.AlgorithmAES256GCM,
+			},
+		})
+		assert.Error(t, err)
+		assert.Equal(t, "unseal failed: new cipher: crypto/aes: invalid key size 0.", err.Error())
+	})
+
 	t.Run("with invalid sealed result", func(t *testing.T) {
 		sealedResult := base64Decode("noXc7VOpBstjjcavDKSKr4HTavt4mdq8h6NC32T0hUtw9S0jXT8lPjZiWL8SyHxmrF3uTGqO+g==")
 		key := base64Decode("p2PA7MGy5tx56cnyJaFZMr96BCFwZeHjZV2EqMvTq53=")
