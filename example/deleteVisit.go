6ddc763d7a799b3719c35bfc1763ac326b095042
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-05-23 19:18:16 +0900

    chore: update deleteVisit example and test

---
+++ example/deleteVisit.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/fingerprintjs/fingerprint-pro-server-api-go-sdk/v5/sdk"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	cfg := sdk.NewConfiguration()
+	client := sdk.NewAPIClient(cfg)
+
+	// Load environment variables
+	godotenv.Load()
+
+	// Default region is sdk.RegionUS
+	if os.Getenv("REGION") == "eu" {
+		cfg.ChangeRegion(sdk.RegionEU)
+	}
+	if os.Getenv("REGION") == "ap" {
+		cfg.ChangeRegion(sdk.RegionAsia)
+	}
+
+	// Configure authorization, in our case with API Key
+	auth := context.WithValue(context.Background(), sdk.ContextAPIKey, sdk.APIKey{
+		Key: os.Getenv("FINGERPRINT_API_KEY"),
+	})
+
+	// Usually this data will come from your frontend app
+	visitorId := os.Getenv("VISITOR_ID")
+
+	// Delete visitor data. If you are interested in using this API, please contact our support team (https://fingerprint.com/support/) to activate it for you
+	httpRes, err := client.FingerprintApi.DeleteVisitorData(auth, visitorId)
+
+	fmt.Printf("%+v\n", httpRes)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
