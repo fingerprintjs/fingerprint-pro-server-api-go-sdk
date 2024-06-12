f1335763f2d0fa9cbe902ab6f6a366070e9b57b9
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-06-12 12:04:14 +0200

    refactor: rename webhook functions

--- docs/Webhook.md
+++ docs/Webhook.md
@@ -1,8 +1,8 @@
 # Webhook
 
-## **CheckHeader**
+## **IsValidWebhookSignature**
 
-> bool CheckHeader(header string, data []byte, secret string)
+> bool IsValidWebhookSignature(header string, data []byte, secret string)
 
 Verifies the HMAC signature extracted from the "fpjs-event-signature" header of the incoming request. This is a part of the webhook signing process, which is available only for enterprise customers.
 If you wish to enable it, please [contact our support](https://fingerprint.com/support).
--- sdk/webhook/signing.go
+++ sdk/webhook/signing.go
@@ -7,23 +7,23 @@ import (
 	"strings"
 )
 
-func checkSignature(signature string, data []byte, secret string) bool {
+func isValidHmacSignature(signature string, data []byte, secret string) bool {
 	h := hmac.New(sha256.New, []byte(secret))
 	h.Write(data)
 	computedSignature := hex.EncodeToString(h.Sum(nil))
 	return signature == computedSignature
 }
 
-// CheckHeader Verifies the HMAC signature extracted from the "fpjs-event-signature" header of the incoming request. This is a part of the webhook signing process, which is available only for enterprise customers.
+// IsValidWebhookSignature Verifies the HMAC signature extracted from the "fpjs-event-signature" header of the incoming request. This is a part of the webhook signing process, which is available only for enterprise customers.
 // If you wish to enable it, please contact our support: https://fingerprint.com/support
-func CheckHeader(header string, data []byte, secret string) bool {
+func IsValidWebhookSignature(header string, data []byte, secret string) bool {
 	signatures := strings.Split(header, ",")
 
 	for _, signature := range signatures {
 		parts := strings.Split(signature, "=")
 		if len(parts) == 2 && parts[0] == "v1" {
 			hash := parts[1]
-			if checkSignature(hash, data, secret) {
+			if isValidHmacSignature(hash, data, secret) {
 				return true
 			}
 		}
