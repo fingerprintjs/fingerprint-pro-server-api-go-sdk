b749ce773a3dd611d6a5e4034cb3684ba10dc759
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-06-21 13:55:58 +0200

    fix: allow passing `nil` configuration to `NewAPIClient`

---
+++ test/api_fingerprint_test.go
@@ -0,0 +1,15 @@
+package test
+
+import (
+	"github.com/fingerprintjs/fingerprint-pro-server-api-go-sdk/v5/sdk"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestApiFingerprint(t *testing.T) {
+	t.Run("Create with empty config", func(t *testing.T) {
+		client := sdk.NewAPIClient(nil)
+
+		assert.NotNil(t, client)
+	})
+}
