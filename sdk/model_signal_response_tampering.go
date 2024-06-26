c29aea98fb7f814f19b46225a5656318fc4f81fd
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-06-26 13:31:26 +0200

    fix: use correct error type for `incognito`, `rawDeviceAttributes` and `tampering` in the `GetEvent` method

--- sdk/model_signal_response_tampering.go
+++ sdk/model_signal_response_tampering.go
@@ -10,6 +10,6 @@
 package sdk
 
 type SignalResponseTampering struct {
-	Data   *TamperingResult `json:"data,omitempty"`
-	Error_ *ProductError    `json:"error,omitempty"`
+	Data   *TamperingResult     `json:"data,omitempty"`
+	Error_ *IdentificationError `json:"error,omitempty"`
 }
