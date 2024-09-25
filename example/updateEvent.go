22efed1967a49e4a97e119993540c788ea6347c3
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-09-25 11:53:38 +0200

    chore: add updateEvent example

---
+++ example/updateEvent.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/fingerprintjs/fingerprint-pro-server-api-go-sdk/v6/sdk"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	cfg := sdk.NewConfiguration()
+	client := sdk.NewAPIClient(cfg)
+
+	// Load environment variables
+	godotenv.Load()
+
+	// Default region is sdk.RegionUS
+	if os.Getenv("REGION") == "eu" {
+		cfg.ChangeRegion(sdk.RegionEU)
+	}
+	if os.Getenv("REGION") == "ap" {
+		cfg.ChangeRegion(sdk.RegionAsia)
+	}
+
+	// Configure authorization, in our case with API Key
+	auth := context.WithValue(context.Background(), sdk.ContextAPIKey, sdk.APIKey{
+		Key: os.Getenv("FINGERPRINT_API_KEY"),
+	})
+
+	// Usually this data will come from your frontend app
+	requestId := os.Getenv("REQUEST_ID")
+	tag := make(map[string]string)
+	tag["key"] = "value"
+	tagAsInterface := interface{}(tag)
+
+	body := sdk.EventUpdateRequest{
+		Suspect:  false,
+		LinkedId: "new_linked_id",
+		Tag:      &tagAsInterface,
+	}
+
+	httpRes, err := client.FingerprintApi.UpdateEvent(auth, body, requestId)
+
+	fmt.Printf("%+v\n", httpRes)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
