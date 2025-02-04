b7cf01d178d3cf61915495cc8d5673a953a72b5e
Przemysław Żydek <przemyslawzydek@gmail.com>
2025-02-04 10:57:33 +0100

    fix: correctly handle nullable values

--- sdk/url_values.go
+++ sdk/url_values.go
@@ -9,24 +9,43 @@ func addMapToUrlValues(data map[string]any, values *url.Values) {
 	for key, value := range data {
 		var stringValue string
 
-		switch v := value.(type) {
+		if value == nil {
+			continue
+		}
 
+		switch v := value.(type) {
 		case bool:
 			stringValue = strconv.FormatBool(v)
 		case *bool:
+			if v == nil {
+				continue
+			}
+
 			stringValue = strconv.FormatBool(*v)
 
 		case *string:
+			if v == nil {
+				continue
+			}
+
 			stringValue = *v
 		case string:
 			stringValue = v
 
 		case *int32:
+			if v == nil {
+				continue
+			}
+
 			stringValue = strconv.FormatInt(int64(*v), 10)
 		case int32:
 			stringValue = strconv.FormatInt(int64(v), 10)
 
 		case *int64:
+			if v == nil {
+				continue
+			}
+
 			stringValue = strconv.FormatInt(*v, 10)
 		case int64:
 			stringValue = strconv.FormatInt(v, 10)
